055d0c1c541be486f7a07a3d1e45711201cefbf7
sh1d3n <jakusz12@interia.pl>
2020-06-18 21:34:17 +0200

    dodanie CarDealer

---
+++ src/com/project/CarDealer.java
@@ -0,0 +1,24 @@
+package com.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarDealer {
+    List<Car> carList = new ArrayList<>();
+
+    public List<Car> getCarList() {
+        return carList;
+    }
+
+    public void setCarList(List<Car> carList) {
+        this.carList = carList;
+    }
+
+    public void buyCar(Car car) {
+
+    }
+
+    public void sellCar(Car car) {
+
+    }
+}
