76c43b7fb93e0954523c65266105068ee0463f55
sh1d3n <jakusz12@interia.pl>
2020-06-24 10:24:03 +0200

    dodanie poprawek

--- src/com/project/Dealerable.java
+++ src/com/project/Dealerable.java
@@ -4,4 +4,9 @@ public interface Dealerable {
     void importCarsFromAbroad(int amountOfCarsToBuy, int priceOfCarsToBuy);
     void buyCarFromSeller(Human seller);
     void sellCarToBuyer(CarDetails carDetails, Human buyer);
+    void listDealerTransactions();
+
+    CarDetails showMostExpensiveCar();
+    CarDetails showCheapestCar();
+    CarDetails showRandomCar();
 }
